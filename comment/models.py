533a063bc71aa05c33e0a7baed129f1fdad73242
Rashisky <rashisky007@gmail.com>
2025-11-23 09:52:23 +0100

    Comment Model

--- comment/models.py
+++ comment/models.py
@@ -6,7 +6,6 @@ from django.contrib.contenttypes.fields import GenericRelation
 from like.models import Like
 
 
-# Create your models here.
 class Comment(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     author = models.ForeignKey(
