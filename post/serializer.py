030a798d344c011e3bd17dad52dad7a050097929
Rashisky <rashisky007@gmail.com>
2025-11-25 18:15:10 +0100

    Update like_count to likes

--- post/serializer.py
+++ post/serializer.py
@@ -6,23 +6,23 @@ from typing import Dict
 
 class CommentSummarySerializer(serializers.ModelSerializer):
     excerpt = serializers.CharField(read_only=True)
-    like_count = serializers.IntegerField(read_only=True)
+    likes = serializers.IntegerField(read_only=True, source='like_count')
     reply_count = serializers.IntegerField(read_only=True)
 
     class Meta:
         model = Comment
-        fields = ['id', 'author', 'excerpt', 'like_count', 'reply_count', 'created_at']
+        fields = ['id', 'author', 'excerpt', 'likes', 'reply_count', 'created_at']
         read_only_fields = fields
 
 
 class PostDetailSerializer(serializers.ModelSerializer):
     top_comments = CommentSummarySerializer(many=True, read_only=True)
     comment_count = serializers.IntegerField(read_only=True)
-    like_count = serializers.IntegerField(read_only=True)
+    likes = serializers.IntegerField(read_only=True, source='like_count')
 
     class Meta:
         model = Post
-        fields = ['isbn', 'author', 'title', 'content', 'is_published', 'like_count', 'comment_count', 'top_comments', 'created_at']
+        fields = ['isbn', 'author', 'title', 'content', 'is_published', 'likes', 'comment_count', 'top_comments', 'created_at']
         read_only_fields = ['isbn', 'author', 'created_at']
 
     def create(self, validated_data: Dict[str, str]):
@@ -38,9 +38,9 @@ class PostDetailSerializer(serializers.ModelSerializer):
 
 class PostListSerializer(serializers.ModelSerializer):
     comment_count = serializers.IntegerField(read_only=True)
-    like_count = serializers.IntegerField(read_only=True)
+    likes = serializers.IntegerField(read_only=True, source='like_count')
     excerpt = serializers.CharField(read_only=True)
 
     class Meta:
         model = Post
-        fields = ['isbn', 'title', 'excerpt', 'author', 'like_count', 'comment_count', 'is_published', 'created_at']
+        fields = ['isbn', 'title', 'excerpt', 'author', 'likes', 'comment_count', 'is_published', 'created_at']
