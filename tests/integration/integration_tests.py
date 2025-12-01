93a510fe4f63036c6e08d2c47f4de5ffcc12495b
Rashisky <rashisky007@gmail.com>
2025-12-01 12:42:20 +0100

    Integration Test Style formatting

--- tests/integration/integration_tests.py
+++ tests/integration/integration_tests.py
@@ -27,8 +27,10 @@ class BlogAPIIntegrationTest(APITestCase):
         cls.post = Post.objects.create(
             author=cls.user1,
             title="Introduction to Politics",
-            content=("Perhaps the greatest issue we are facing in world"
-            "has everything to do with politics"),
+            content=(
+                "Perhaps the greatest issue we are facing in world"
+                "has everything to do with politics"
+            ),
             is_published=True,
         )
         cls.draft = Post.objects.create(
@@ -38,16 +40,20 @@ class BlogAPIIntegrationTest(APITestCase):
         )
         cls.comment = Comment.objects.create(
             author=cls.user2,
-            content=("I couldn't agree more to this. The number of gullible "
-            "people in this country is beyond comprehension"),
+            content=(
+                "I couldn't agree more to this. The number of gullible "
+                "people in this country is beyond comprehension"
+            ),
             post=cls.post,
         )
         cls.reply = Comment.objects.create(
             author=cls.user1,
             parent=cls.comment,
             post=cls.post,
-            content=("Of course. It's unfortunate that people's intelligence "
-            "couldn't be tested before voting"),
+            content=(
+                "Of course. It's unfortunate that people's intelligence "
+                "couldn't be tested before voting"
+            ),
         )
         cls.like_post = Like.objects.create(
             user=cls.user2,
