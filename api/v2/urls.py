84716cab355f47941ef03e064c25155dc1a16def
Rashisky <rashisky007@gmail.com>
2026-01-02 22:02:09 +0100

    Version 2 urls

--- api/v2/urls.py
+++ api/v2/urls.py
@@ -1,5 +1,19 @@
-from django.urls import path
+from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+    SpectacularAPIView
+)
 
 urlpatterns = [
+    path("", include("api.v2.comment.urls")),
+    path("", include("app.core.authentication.urls")),
+    path("", include("api.v2.like.urls")),
 
+    path("posts/", include("api.v2.post.urls")),
+    path("users/", include("api.v2.user.urls")),
+
+    path("schema/swagger/", SpectacularSwaggerView.as_view(), name="swagger"),
+    path("schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc")
 ]
