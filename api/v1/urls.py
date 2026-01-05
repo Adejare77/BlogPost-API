ddef12b72dee7d6edd983531dd41e53ef4e8845b
Rashisky <rashisky007@gmail.com>
2026-01-05 21:08:44 +0100

    add app_name to distinct from version 2

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -2,16 +2,17 @@ from django.urls import path, include
 from drf_spectacular.views import (
     SpectacularAPIView,
     SpectacularRedocView,
-    SpectacularSwaggerView
+    SpectacularSwaggerView,
 )
 
-urlpatterns=[
+app_name = "v1"
+
+urlpatterns = [
     path("", include("api.v1.comment.urls")),
     path("", include("app.core.authentication.urls")),
     path("", include("api.v1.like.urls")),
     path("posts/", include("api.v1.post.urls")),
     path("users/", include("api.v1.user.urls")),
-
     path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
     path("schema/swagger/", SpectacularSwaggerView.as_view(), name="swagger"),
