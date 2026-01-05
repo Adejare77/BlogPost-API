63ed3650d53130031c22a9c6da39928923c02844
Rashisky <rashisky007@gmail.com>
2026-01-05 21:11:13 +0100

    Add app_name to differentiate from other versions

--- api/v2/comment/urls.py
+++ api/v2/comment/urls.py
@@ -4,14 +4,28 @@ from api.v2.comment.views import (
     CommentListCreateAPIView,
     CommentRetrieveUpdateDestroyAPIView,
     ReplyListCreateAPIView,
-    ReplyRetrieveUpdateDestroyAPIView
+    ReplyRetrieveUpdateDestroyAPIView,
 )
 
 urlpatterns = [
-    path("posts/<uuid:post_id>/comments/", CommentListCreateAPIView.as_view(), name="comments"),
     path(
-        "comments/<uuid:comment_id>/", CommentRetrieveUpdateDestroyAPIView.as_view(), name="comment-detail"
+        "posts/<uuid:post_id>/comments/",
+        CommentListCreateAPIView.as_view(),
+        name="comments",
     ),
-    path("comments/<uuid:comment_id>/replies/", ReplyListCreateAPIView.as_view(), name="replies"),
-    path("replies/<uuid:reply_id>/", ReplyRetrieveUpdateDestroyAPIView.as_view(), name="reply-detail"),
+    path(
+        "comments/<uuid:comment_id>/",
+        CommentRetrieveUpdateDestroyAPIView.as_view(),
+        name="comment-detail",
+    ),
+    path(
+        "comments/<uuid:comment_id>/replies/",
+        ReplyListCreateAPIView.as_view(),
+        name="replies",
+    ),
+    path(
+        "replies/<uuid:reply_id>/",
+        ReplyRetrieveUpdateDestroyAPIView.as_view(),
+        name="reply-detail",
+    ),
 ]
