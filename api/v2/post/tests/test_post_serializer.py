1080d0b75963ee85f3fef2e08e4d4fde3d76d821
Rashisky <rashisky007@gmail.com>
2026-01-05 21:07:18 +0100

    conftest, view uittest and serializer tests for post

--- api/v2/post/tests/test_post_serializer.py
+++ api/v2/post/tests/test_post_serializer.py
@@ -1,7 +1,7 @@
 from api.v2.post.serializer import (
     PostListSerializer,
     PostCreateSerializer,
-    PostDetailSerializer
+    PostDetailSerializer,
 )
 
 from app.post.models import Post
@@ -12,69 +12,55 @@ from rest_framework import serializers
 
 class TestPostCreateSerializer:
     def test_valid_data_validates_successfully(self):
-        data = {
-            'content': 'Valid Content',
-            'title': 'Valid Title'
-        }
+        data = {"content": "Valid Content", "title": "Valid Title"}
 
         serializer = PostCreateSerializer(data=data)
         assert serializer.is_valid() is True
 
     def test_missing_title_fails_validation(self):
         data = {
-            'content': 'Valid Content',
+            "content": "Valid Content",
         }
 
         serializer = PostCreateSerializer(data=data)
         assert serializer.is_valid() is False
 
     def test_empty_title_fails_validation(self):
-        data = {
-            'content': 'Valid Content',
-            'title': ''
-        }
+        data = {"content": "Valid Content", "title": ""}
 
         serializer = PostCreateSerializer(data=data)
         assert serializer.is_valid() is False
 
     def test_missing_content_fails_validation(self):
-        data = {
-            'title': 'Valid title',
-            'is_published': False
-        }
+        data = {"title": "Valid title", "is_published": False}
 
         serializer = PostCreateSerializer(data=data)
         assert serializer.is_valid() is False
 
     def test_empty_content_fails_validation(self):
-        data = {
-            'title': 'Valid title',
-            'content': '',
-            'is_published': True
-        }
+        data = {"title": "Valid title", "content": "", "is_published": True}
 
         serializer = PostCreateSerializer(data=data)
         assert serializer.is_valid() is False
 
     def test_missing_is_published_defaults_to_false(self, users, posts):
         data = {
-            'title': 'Valid title',
-            'content': 'Valid Content',
+            "title": "Valid title",
+            "content": "Valid Content",
         }
 
         serializer = PostCreateSerializer(data=data)
         assert serializer.is_valid() is True
 
-        post = serializer.save(author=users['user_1'])
+        post = serializer.save(author=users["user_1"])
 
         assert post.is_published == False
 
-
     def test_is_published_only_accepts_bool(self):
         data = {
-            'title': 'Valid title',
-            'content': 'valid content',
-            'is_published': 'invalid_status'
+            "title": "Valid title",
+            "content": "valid content",
+            "is_published": "invalid_status",
         }
 
         serializer = PostCreateSerializer(data=data)
@@ -82,19 +68,24 @@ class TestPostCreateSerializer:
 
     def test_serialization_includes_expected_fields(self, users):
         data = {
-            'title': 'Valid title',
-            'content': 'Valid Content',
-            'is_published': True
+            "title": "Valid title",
+            "content": "Valid Content",
+            "is_published": True,
         }
 
         serializer = PostCreateSerializer(data=data)
         assert serializer.is_valid() is True
 
-        post = serializer.save(author=users['user_2'])
+        post = serializer.save(author=users["user_2"])
 
         model_fields = {f.name for f in post._meta.get_fields()}
         expected_field = {
-            'id', 'author', 'content', 'title', 'is_published', 'created_at'
+            "id",
+            "author",
+            "content",
+            "title",
+            "is_published",
+            "created_at",
         }
 
         assert expected_field.issubset(model_fields)
@@ -106,8 +97,14 @@ class TestPostCreateSerializer:
 class TestPostListSerializer:
     def test_serialization_includes_expected_fields(self, db):
         expected_fields = {
-            'id', 'author', 'title', 'excerpt', 'likes', 'comment_count',
-            'is_published', 'created_at'
+            "id",
+            "author",
+            "title",
+            "excerpt",
+            "likes",
+            "comment_count",
+            "is_published",
+            "created_at",
         }
 
         posts = Post.objects.all()
@@ -119,37 +116,41 @@ class TestPostListSerializer:
     def test_serialization_fields_are_read_only(self):
         ser = PostListSerializer()
 
-        assert(field.read_only for field in ser.fields.values())
+        writable_fields = {
+            name for name, field in ser.fields.items() if not field.read_only
+        }
+
+        assert writable_fields == set()
 
 
 class TestPostDetailSerializer:
     def test_content_field_can_be_updated(self, posts):
-        post = posts['post_1']
-        data = {'content': 'This is the latest content update'}
+        post = posts["post_1"]
+        data = {"content": "This is the latest content update"}
 
         ser = PostDetailSerializer(instance=post, data=data, partial=True)
         ser.is_valid()
         ser.save()
 
         post.refresh_from_db()
-        assert post.content == data['content']
+        assert post.content == data["content"]
 
     def test_blank_content_cannot_be_updated(self, posts):
-        post = posts['post_1']
-        data = {'content': ''}
+        post = posts["post_1"]
+        data = {"content": ""}
 
         ser = PostDetailSerializer(instance=post, data=data, partial=True)
 
         with pytest.raises(serializers.ValidationError) as exc_info:
             ser.is_valid(raise_exception=True)
 
-        assert 'content' in exc_info.value.detail
-        assert exc_info.value.detail['content'][0] == 'content field cannot be empty'
+        assert "content" in exc_info.value.detail
+        assert exc_info.value.detail["content"][0] == "content field cannot be empty"
 
     def test_title_field_can_be_updated(self, posts):
-        post = posts['post_1']
+        post = posts["post_1"]
 
-        data = {'title': 'This is the latest title update'}
+        data = {"title": "This is the latest title update"}
 
         ser = PostDetailSerializer(instance=post, data=data, partial=True)
         ser.is_valid()
@@ -157,55 +158,50 @@ class TestPostDetailSerializer:
 
         post.refresh_from_db()
 
-        assert post.title == data['title']
+        assert post.title == data["title"]
 
     def test_blank_title_cannot_be_updated(self, posts):
-        post = posts['post_1']
-        data = {'title': ''}
+        post = posts["post_1"]
+        data = {"title": ""}
 
         ser = PostDetailSerializer(instance=post, data=data, partial=True)
 
         with pytest.raises(serializers.ValidationError) as exc_info:
             ser.is_valid(raise_exception=True)
 
-        assert 'title' in exc_info.value.detail
-        assert exc_info.value.detail['title'][0] == 'title field cannot be empty'
+        assert "title" in exc_info.value.detail
+        assert exc_info.value.detail["title"][0] == "title field cannot be empty"
 
     def test_is_published_field_can_be_updated(self, posts):
-        post = posts['post_1']
-
-        data = {'is_published': False}
-
+        post = posts["post_1"]
+        data = {"is_published": False}
         ser = PostDetailSerializer(instance=post, data=data, partial=True)
-        ser.is_valid()
+
+        assert ser.is_valid(), ser.errors
         ser.save()
 
         post.refresh_from_db()
-
-        assert post.is_published == data['is_published']
+        assert post.is_published == data["is_published"]
 
     def test_blank_is_published_cannot_be_updated(self, posts):
-        post = posts['post_1']
-        old_is_published = post.is_published
-
-        data = {'is_published': ''}
-
+        post = posts["post_1"]
+        data = {"is_published": ""}
         ser = PostDetailSerializer(instance=post, data=data, partial=True)
 
         with pytest.raises(serializers.ValidationError) as exc_info:
             ser.is_valid(raise_exception=True)
 
-        assert 'is_published' in exc_info.value.detail
-        assert exc_info.value.detail['is_published']
+        assert "is_published" in exc_info.value.detail
+        assert exc_info.value.detail["is_published"]
 
     def test_title_content_is_published_fields_can_be_updated(self, posts):
-        post = posts['post_1']
+        post = posts["post_1"]
 
         data = {
-            'title': 'This is the latest title update',
-            'content': 'This is the latest content update',
-            'is_published': False,
-            }
+            "title": "This is the latest title update",
+            "content": "This is the latest content update",
+            "is_published": False,
+        }
 
         ser = PostDetailSerializer(instance=post, data=data, partial=True)
         ser.is_valid()
@@ -213,26 +209,17 @@ class TestPostDetailSerializer:
 
         post.refresh_from_db()
 
-        assert post.title == data['title']
-        assert post.content == data['content']
-        assert post.is_published == data['is_published']
-
+        assert post.title == data["title"]
+        assert post.content == data["content"]
+        assert post.is_published == data["is_published"]
 
     def test_read_only_fields(self, posts):
-        post = Post.objects.first()
-        post.like_count = 293
-        post.top_comments = []
-        post.comment_count = 32
-
-        read_only_fields = {
-            'id', 'author', 'likes', 'comment_count',
-            'top_comments', 'created_at'
+        ser = PostDetailSerializer()
+        writable_fields = {
+            name for name, field in ser.fields.items() if not field.read_only
         }
 
-        ser = PostDetailSerializer(instance=post)
-
-        assert read_only_fields == set(ser.Meta.read_only_fields)
-
+        assert writable_fields == {"content", "is_published", "title"}
 
     def test_read_only_fields_cannot_be_updated(self, posts):
         post = Post.objects.first()
@@ -245,9 +232,13 @@ class TestPostDetailSerializer:
         old_top_comments = post.top_comments = []
         old_comment_count = post.comment_count = 3
         data = {
-            'id': '898393', 'author': user, 'created_at': '05-01-2026',
-            'likes': 23, 'top_comments': [], 'comment_count': 12
-            }
+            "id": "898393",
+            "author": user,
+            "created_at": "05-01-2026",
+            "likes": 23,
+            "top_comments": [],
+            "comment_count": 12,
+        }
 
         ser = PostDetailSerializer(instance=post, data=data)
         ser.is_valid()
@@ -260,10 +251,9 @@ class TestPostDetailSerializer:
         assert post.comment_count == old_comment_count
 
     def test_likes_maps_to_like_count(self, posts):
-        post = posts['post_2']
-        likes = post.like_count = 932
+        post = posts["post_2"]
+        post.like_count = 932
 
         ser = PostDetailSerializer(instance=post)
 
-        assert ser.data['likes'] == likes
-
+        assert ser.data["likes"] == 932
