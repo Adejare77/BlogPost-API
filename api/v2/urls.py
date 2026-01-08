34f9a526e4cc5fa06b9968a98abaf7be89fc05fb
Rashisky <rashisky007@gmail.com>
2026-01-08 18:19:30 +0100

    Adjust swagger UI endpoints

--- api/v2/urls.py
+++ api/v2/urls.py
@@ -13,7 +13,8 @@ urlpatterns = [
     path("", include("api.v2.like.urls")),
     path("posts/", include("api.v2.post.urls")),
     path("users/", include("api.v2.user.urls")),
-    path("schema/swagger/", SpectacularSwaggerView.as_view(), name="swagger"),
     path("schema/", SpectacularAPIView.as_view(), name="schema"),
-    path("redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path("schema/swagger/", SpectacularSwaggerView.as_view(url_name="v2:schema"), name="swagger"),
+    path("redoc/", SpectacularRedocView.as_view(url_name="v2:schema"), name="redoc"),
+
 ]
