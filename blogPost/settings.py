75b9da73780f13cf7ef2825c2f980ddb2df5e0f9
Rashisky <rashisky007@gmail.com>
2026-01-22 13:31:44 +0100

    include localhost in allowed_hosts

--- blogPost/settings.py
+++ blogPost/settings.py
@@ -18,7 +18,7 @@ import environ
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-env = environ.Env(DEBUG=(bool, True), TESTING=(bool, False))
+env = environ.Env(DEBUG=(bool, False), TESTING=(bool, False))
 
 # read the .env file
 environ.Env.read_env(os.path.join(BASE_DIR, ".env"))
@@ -33,7 +33,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost"]
 
 
 # Application definition
