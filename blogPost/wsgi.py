c7955c43156b6e4af807839626ba6a1773af72cd
Rashisky <rashisky007@gmail.com>
2026-01-29 19:31:11 +0100

    set DJANGO_SETTINGS_MODULE to blogPost.settings.base

--- blogPost/wsgi.py
+++ blogPost/wsgi.py
@@ -10,6 +10,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogPost.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "blogPost.settings.base")
 
 application = get_wsgi_application()
