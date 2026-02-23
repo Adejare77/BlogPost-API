6c80675959952063158a62c6e757f2a49bf56e54
Rashisky <rashisky007@gmail.com>
2026-02-23 19:30:49 +0100

    Prometheus health check url

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,6 +1,5 @@
 from django.urls import path
+
 from app.core.views import HealthCheck
 
-urlpatterns = [
-    path("", HealthCheck.as_view(), name="health-check")
-]
+urlpatterns = [path("", HealthCheck.as_view(), name="health-check")]
