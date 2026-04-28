216bbcab89483a7311588e1b78965dd150af54a5
Rashisky <rashisky007@gmail.com>
2026-04-28 17:06:18 +0100

    auto reload on changes and capture outputs

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,4 +6,6 @@ exec gunicorn blogPost.wsgi:application \
     --bind 0.0.0.0:8000 \
     --workers 3 \
     --timeout 30 \
-    --keep-alive 2
+    --keep-alive 2 \
+    --capture-output \
+    --reload
